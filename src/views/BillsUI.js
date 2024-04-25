b240c7721ddfcb29bfaaabb8b7df44d6af80306c
Epaulais Rémi <epaulais.remi@gmail.com>
2024-04-25 12:46:34 +0200

    Corrected Bills bug

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -5,7 +5,7 @@ import LoadingPage from "./LoadingPage.js";
 import Actions from "./Actions.js";
 
 const row = (bill) => {
-  return `
+    return `
     <tr>
       <td>${bill.type}</td>
       <td>${bill.name}</td>
@@ -20,15 +20,16 @@ const row = (bill) => {
 };
 
 const rows = (data) => {
-  const sortedData =
-    data && data.length
-      ? data.sort((a, b) => b.date.localeCompare(a.date))
-      : [];
-  return sortedData.map((bill) => row(bill)).join("");
+    return data && data.length
+        ? data
+              .sort((a, b) => new Date(b.date) - new Date(a.date))
+              .map((bill) => row(bill))
+              .join("")
+        : "";
 };
 
 export default ({ data: bills, loading, error }) => {
-  const modal = () => `
+    const modal = () => `
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
@@ -45,13 +46,13 @@ export default ({ data: bills, loading, error }) => {
     </div>
   `;
 
-  if (loading) {
-    return LoadingPage();
-  } else if (error) {
-    return ErrorPage(error);
-  }
+    if (loading) {
+        return LoadingPage();
+    } else if (error) {
+        return ErrorPage(error);
+    }
 
-  return `
+    return `
     <div class='layout'>
       ${VerticalLayout(120)}
       <div class='content'>
