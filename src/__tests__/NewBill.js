90480bb8e7ba3fbd5a06b5d4c2c031006a208b74
Rémi Kiovo <kiovo.an@gmail.com>
2024-04-30 10:50:04 +0200

    Bills test statement 100%

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,34 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import "@testing-library/jest-dom";
+import { fireEvent, screen } from "@testing-library/dom";
+import NewBillUI from "../views/NewBillUI.js";
+import NewBill from "../containers/NewBill.js";
+import BillsUI from "../views/BillsUI";
+import { localStorageMock } from "../__mocks__/localStorage";
+import { ROUTES } from "../constants/routes";
+import store from "../__mocks__/store";
+import Store from "../app/Store";
 
+const onNavigate = (pathname) => {
+    document.body.innerHTML = ROUTES({ pathname });
+};
+
+Object.defineProperty(window, "LocalStorage", { value: localStorageMock });
+window.localStorage.setItem(
+    "user",
+    JSON.stringify({
+        type: "Employee",
+    })
+);
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+    describe("When I am on NewBill Page", () => {
+        test("Then ...", () => {
+            const html = NewBillUI();
+            document.body.innerHTML = html;
+            //to-do write assertion
+        });
+    });
+});
